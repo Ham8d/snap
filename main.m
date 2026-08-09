0ae549101026d47e107364eaeacfb014cb4362a2
Ham8d <07813644874m@gmail.com>
2026-08-09 06:52:43 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,59 +1,74 @@
 #import "substrate.h"
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
 
-// تعريف كلاس افتراضي لسناب (قد يختلف حسب النسخة)
+// تعريف الدوال الأساسية من Foundation يدوياً لتجنب الـ Headers الثقيلة
+void NSLog(NSString *format, ...);
+
+// تعريف الكلاسات الهدف في سناب
+// ملاحظة: يجب التأكد من أسماء الكلاسات والدوال من ملف سناب الأصلي
+// هذه أسماء شائعة في سناب، قد تحتاج لتعديلها حسب النسخة
+
 @interface SUBSubscriptionManager : NSObject
-- (BOOL)isPremiumActive;
-- (void)requestSubscriptionCodeWithCompletion:(void(^)(BOOL success, NSString *error))completion;
-- (void)validateCode:(NSString *)code completion:(void(^)(BOOL valid))completion;
 @end
 
 @interface SUBAppController : NSObject
-- (BOOL)isSubscribed;
 @end
 
-// Hook للدالة التي تحقق من الاشتراك
-%hook SUBSubscriptionManager
-
-- (BOOL)isPremiumActive {
-    NSLog(@"[SnapFix] isPremiumActive called. Returning YES.");
+// دالة مساعدة للـ Hook
+static BOOL isPremiumOverride(id self, SEL _cmd) {
+    NSLog(@"[SnapFix] Hooked: isPremiumActive -> YES");
     return YES;
 }
 
-- (void)requestSubscriptionCodeWithCompletion:(void(^)(BOOL success, NSString *error))completion {
-    NSLog(@"[SnapFix] requestSubscriptionCode intercepted.");
+static void requestSubscriptionOverride(id self, SEL _cmd, void (^completion)(BOOL, id)) {
+    NSLog(@"[SnapFix] Hooked: requestSubscriptionCodeWithCompletion -> Success");
     if (completion) {
         completion(YES, nil);
-    }
 }
 
-- (void)validateCode:(NSString *)code completion:(void(^)(BOOL valid))completion {
-    NSLog(@"[SnapFix] validateCode intercepted for code: %@", code);
+static void validateCodeOverride(id self, SEL _cmd, id code, void (^completion)(BOOL)) {
+    NSLog(@"[SnapFix] Hooked: validateCode -> Valid");
     if (completion) {
         completion(YES);
-    }
 }
 
-%end
-
-// Hook للكلاس الرئيسي إذا كان يستخدمه سناب
-%hook SUBAppController
-
-- (BOOL)isSubscribed {
-    NSLog(@"[SnapFix] SUBAppController isSubscribed called. Returning YES.");
+static BOOL isSubscribedOverride(id self, SEL _cmd) {
+    NSLog(@"[SnapFix] Hooked: SUBAppController.isSubscribed -> YES");
     return YES;
 }
 
-%end
+%ctor {
+    NSLog(@"[SnapFix] Library Loaded!");
 
-// دالة التهيئة
-__attribute__((constructor))
-void initialize() {
-    NSLog(@"[SnapFix] CydiaSubstrate Library Loaded Successfully!");
-    
-    // اختياري: تأخير التحميل قليلاً لتجنب الكراش في بداية التطبيق
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC), dispatch_get_main_queue()), ^{
-        NSLog(@"[SnapFix] Delayed initialization complete.");
-    });
+    // 1. Hook لـ SUBSubscriptionManager
+    Class subMgrClass = objc_getClass("SUBSubscriptionManager");
+    if (subMgrClass) {
+        // Hook لدالة isPremiumActive
+        Method isPremiumMethod = class_getInstanceMethod(subMgrClass, @selector(isPremiumActive));
+        if (isPremiumMethod) {
+            method_setImplementation(isPremiumMethod, (IMP)isPremiumOverride);
+        }
+
+        // Hook لدالة requestSubscriptionCodeWithCompletion:
+        Method reqMethod = class_getInstanceMethod(subMgrClass, @selector(requestSubscriptionCodeWithCompletion:));
+        if (reqMethod) {
+            method_setImplementation(reqMethod, (IMP)requestSubscriptionOverride);
+        }
+
+        // Hook لدالة validateCode:completion:
+        Method validateMethod = class_getInstanceMethod(subMgrClass, @selector(validateCode:completion:));
+        if (validateMethod) {
+            method_setImplementation(validateMethod, (IMP)validateCodeOverride);
+        }
+    }
+
+    // 2. Hook لـ SUBAppController
+    Class appCtrlClass = objc_getClass("SUBAppController");
+    if (appCtrlClass) {
+        Method isSubMethod = class_getInstanceMethod(appCtrlClass, @selector(isSubscribed));
+        if (isSubMethod) {
+            method_setImplementation(isSubMethod, (IMP)isSubscribedOverride);
+        }
+    }
 }
